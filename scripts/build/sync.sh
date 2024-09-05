3e2eff4ba95df5cab40d53739d2a405512e80d8e
Azure_Stars <zyj578335934@gmail.com>
2024-09-05 14:46:20 +0800

    test7

--- scripts/build/sync.sh
+++ scripts/build/sync.sh
@@ -1,8 +1,8 @@
 ACTOR=$1
 ACTOR_ID=$2
 
-git config user.name $ACTOR
-git config user.email "$ACTOR_ID+$ACTOR@users.noreply.github.com"
+git config --global user.name $ACTOR
+git config --global user.email "$ACTOR_ID+$ACTOR@users.noreply.github.com"
 source /tmp/git-subrepo/.rc
 source ./scripts/build/repo.sh
 CloneRepoConfig
